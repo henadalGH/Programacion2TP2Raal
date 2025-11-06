301d94f3a2a25b02934f104d20b231474c5bec71
henadalGH <hrnnnadal@gmail.com>
2025-11-06 11:17:38 -0300

    creacion del canAtivate

--- src/app/auth/auth.guard.service.ts
+++ src/app/auth/auth.guard.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth-service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AithGuardService implements CanActivate {
+
+authServi = inject(AuthService);
+router = inject(Router);
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return false;
+    console.log({route , state});
+    
+    return this.authServi.isLooggedIn.pipe(
+      map((isLooggedIn: string)=> {
+        if(isLooggedIn==''){
+          this.router.navigate(['/login'])
+          return false;
+        }
+
+        return true;
+      }
+    )
+    )
   }
   
 }
